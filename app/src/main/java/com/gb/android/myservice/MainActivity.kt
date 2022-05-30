75208222be51a0bd5d3053752f495efc0f387d70
mafusail <mikelnord@gmail.com>
2022-05-30 17:29:32 +1000

    add SysService

--- app/src/main/java/com/gb/android/myservice/MainActivity.kt
+++ app/src/main/java/com/gb/android/myservice/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.gb.android.myservice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gb.android.myservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.startIntentServiceButton.setOnClickListener{
+            SysIntentService.startToastJob(this,10)
+        }
+
     }
+
+
+
 }
---
+++ app/src/main/java/com/gb/android/myservice/SysIntentService.kt
@@ -0,0 +1,57 @@
+package com.gb.android.myservice
+
+import android.app.IntentService
+import android.content.Intent
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+
+private const val TAG = "SysIntentService"
+
+class SysIntentService : IntentService("SysIntentService") {
+
+    companion object {
+        private const val SYS_KEY = "SYS_KEY"
+
+        fun startToastJob(context: Context, counter: Int) {
+            val serviceIntent = Intent(context, SysIntentService::class.java)
+            serviceIntent.putExtra(SYS_KEY, counter)
+            context.startService(serviceIntent)
+            Log.d(TAG, "startToastJob() called with: context = $context, counter = $counter")
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d(TAG, "onCreate() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
+    override fun onHandleIntent(intent: Intent?) {
+        Log.d(TAG, "onHandleIntent() called with: intent = $intent")
+        val counter = intent?.getIntExtra(SYS_KEY, 0)
+        Thread.sleep(5_000)
+        when (counter) {
+            0 -> {
+                Handler(Looper.getMainLooper()).post {
+                    Toast.makeText(this, "Counter is empty", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else -> {
+                for (i in 1..counter!!) {
+                    Handler(Looper.getMainLooper()).post {
+                        Toast.makeText(this, "$i", Toast.LENGTH_SHORT).show()
+                    }
+                    Thread.sleep(1_000)
+                }
+            }
+        }
+    }
+
+}
